0b33d712656c514060c11dd97aef688c40865809
Yehor Makarenko <mak.yegor@gmail.com>
2024-10-30 13:22:34 +0200

    Redirect user from Profile page to Login page if not auth

--- src/modules/user/screens/Profile.tsx
+++ src/modules/user/screens/Profile.tsx
@@ -1,10 +1,20 @@
 "use client";
 
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { useCurrentUser } from "../hooks";
 import { LogoutButton } from "@modules/auth/components";
+import { useRouter } from "next/navigation";
 
 export const ProfileScreen: FC = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const tokenCookie = document.cookie.split('; ').find(row => row.startsWith('accessToken='));    
+    if (!tokenCookie || tokenCookie.split('=')[1] === '') {
+      router.push('/login');
+    }
+  }, []);
+
   const { user } = useCurrentUser();
 
   return (
