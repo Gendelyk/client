041f58e541c2d933f02154a72c71190f03d1809e
Yehor Makarenko <mak.yegor@gmail.com>
2024-11-15 12:41:58 +0200

    Added relume

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@relume_io/relume-ui/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  presets: [require("@relume_io/relume-tailwind")],
+  plugins: [],
+};
+export default config;
